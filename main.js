d1563df64860db9c7e3ac55d722ca2f8abb2cea8
Erickvidolin <102288097+Erickvidolin@users.noreply.github.com>
2022-08-06 17:32:21 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function openModal(mn) {
+
+  var modal = document.getElementById(mn);
+
+  
+
+  if (typeof modal == 'undefined' || modal === null)
+  return;
+
+  modal.style.display = 'block';
+
+}
+
+function closeModal(mn){
+
+  let modal = document.getElementById(mn);
+
+  if (typeof modal == 'undefined' || modal === null)
+  return;
+
+  modal.style.display = 'none';
+
+}
+
